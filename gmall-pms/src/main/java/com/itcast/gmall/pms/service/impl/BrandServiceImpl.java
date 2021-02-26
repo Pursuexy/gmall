be189c2616ca87313efa0274f424d03860b84360
oxygen <1365409209@qq.com>
2021-02-26 23:46:26 +0800

    项目初始化

---
+++ gmall-pms/src/main/java/com/itcast/gmall/pms/service/impl/BrandServiceImpl.java
@@ -0,0 +1,20 @@
+package com.itcast.gmall.pms.service.impl;
+
+import com.itcast.gmall.pms.entity.Brand;
+import com.itcast.gmall.pms.mapper.BrandMapper;
+import com.itcast.gmall.pms.service.BrandService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 品牌表 服务实现类
+ * </p>
+ *
+ * @author Pursuexy
+ * @since 2021-02-26
+ */
+@Service
+public class BrandServiceImpl extends ServiceImpl<BrandMapper, Brand> implements BrandService {
+
+}
